b0cb012fa70a9297d049ba7a39b2ddadbbbbb600
Dean <138621406+deantub@users.noreply.github.com>
2024-01-15 00:30:11 +0200

    2

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,7 +13,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.setting.screen_width = self.screen.get_rect().width
+        self.setting.screen_heigth = self.screen.get_rect().heigth
         pygame.display.set_caption("Alien Invasion")
         
         # Задати колір фону
@@ -34,18 +36,26 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        # Перемістити корабель праворуч.
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
-
+                    self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
-                    
+                    self._check_keyup_events(event)
+        
+        def _check_keydown_events(self, event):
+            """Реагувати на натискання клавіш"""
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+            elif event.key == pygame.K_q:
+                sys.exit()
+        
+        def _check_keyup_events(self, event):
+            """Реагувати, коли клавіша не натиснута"""
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = False
+
         def _update_screen(self):
             """Оновити зображення на екрані та перемкнутися на новий екран"""
             self.screen.fill(self.settings.bg_color)
