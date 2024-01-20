662c56a22d746b18650ba07fde5e8277741128d5
Dean <138621406+deantub@users.noreply.github.com>
2024-01-20 23:53:23 +0200

    bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Загальний клас, що керує ресурсами та поведінкою гри"""
@@ -22,47 +23,68 @@ class AlienInvasion:
         self.bg_color = (230, 230, 230)
         
         self.ship = Ship(self)
+        self.bullet = pygame.sprite.Group()
     
     def run_game(self):
         """Розпочати головний цикл гри"""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
-            
-        def _check_events(self):
-            """Реагувати на натискання клавіш та події миші"""
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    self._check_keydown_events(event)
-                elif event.type == pygame.KEYUP:
-                    self._check_keyup_events(event)
-        
-        def _check_keydown_events(self, event):
-            """Реагувати на натискання клавіш"""
-            if event.key == pygame.K_RIGHT:
-                self.ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                self.ship.moving_left = True
-            elif event.key == pygame.K_q:
-                sys.exit()
-        
-        def _check_keyup_events(self, event):
-            """Реагувати, коли клавіша не натиснута"""
-            if event.key == pygame.K_RIGHT:
-                self.ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                self.ship.moving_left = False
 
-        def _update_screen(self):
-            """Оновити зображення на екрані та перемкнутися на новий екран"""
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            
-            # Показати останній намальований екран.
-            pygame.display.flip()
+    def _check_events(self):
+        """Реагувати на натискання клавіш та події миші"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+        
+    def _check_keydown_events(self, event):
+        """Реагувати на натискання клавіш"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        
+    def _check_keyup_events(self, event):
+        """Реагувати, коли клавіша не натиснута"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+    
+    def _fire_bullet(self):
+        """Створити нову кулю та додати її до групи куль"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    
+    def _update_bullets(self):
+        """Оновити позицію куль та позбавитися старих куль"""
+        # Оновити позиції куль.
+        self.bullets.update()
+
+        # Позбутися куль, що зникли
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
+        """Оновити зображення на екрані та перемкнутися на новий екран"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        # Показати останній намальований екран.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Створити екземпляр гри та запустити гру.
