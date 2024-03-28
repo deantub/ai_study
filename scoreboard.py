4e739e3bf3127ed7c8f5c13c161cd10be21a215e
Dean <138621406+deantub@users.noreply.github.com>
2024-03-28 17:10:57 +0200

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -4,7 +4,7 @@ from pygame.sprite import Group
 from ship import Ship
 
 class Scoreboard:
-    """Клас, що виводить рахунок."""
+    """Клас, що виводить рахунок та іншу інформацію на екрані."""
 
     def __init__(self, ai_game):
         """Ініціалізація атрибутів, пов'язаних із рахунком."""
@@ -14,15 +14,14 @@ class Scoreboard:
         self.settings = ai_game.settings
         self.stats = ai_game.stats
 
-        # Налаштування шрифту для відображення рахунку.
+        # Налаштування шрифту для відображення рахунку та рівня.
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Підготувати зображення з початковим рахунком.
+        # Підготувати зображення з початковим рахунком та рівнем.
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
-        self.prep_ships()
 
     def prep_score(self):
         """Перетворити рахунок на зображення."""
@@ -48,7 +47,7 @@ class Scoreboard:
         high_score_str = "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
 
-        # Відцентрувати рекорд по горизонталі
+        # Відцентрувати рекорд по центру верхнього краю екрана.
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
@@ -68,12 +67,3 @@ class Scoreboard:
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
-    
-    def prep_ships(self):
-        """Показує, скільки лишилося кораблів."""
-        self.ships = Group()
-        for ship_number in range(self.stats.ships_left):
-            ship = Ship(self.ai_game)
-            ship.rect.x = 10 + ship_number * ship.rect.width
-            ship.rect.y = 10
-            self.ships.add(ship)
